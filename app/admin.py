c61938f523687aa6b9a0edae64ec15987d25efc9
Peng Yu <yp1990@gmail.com>
2024-04-22 12:32:00 +0100

    display image fields in admin

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from . import models
 
 
@@ -9,25 +10,69 @@ admin.site.register(models.PreferedDestinationType)
 
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'sort', 'url', 'type']
+    list_display = ['image', 'title', 'description', 'sort', 'type']
+
+    def image(self, image):
+        """ display image """
+        return format_html('<img src="{}" class="thumbnail"/>',
+                           image.url)
+
+    class Media:
+        """ load style files """
+        css = {
+            'all': ['app/css/styles.css']
+        }
 
 
 # register UserProfile to admin
 @admin.register(models.UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'avatar_image', 'profile', 'slogan']
+    list_display = ['user', 'iamge', 'profile', 'slogan']
+
+    def iamge(self, profile):
+        """ display image """
+        return format_html('<img src="{}" class="avatar_thumbnail"/>',
+                           profile.avatar_image.url)
+
+    class Media:
+        """ load style files """
+        css = {
+            'all': ['app/css/styles.css']
+        }
 
 
 # register Post to admin
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'summary', 'created_at']
+    list_display = ['title', 'user', 'summary', 'created_at', 'cover_image']
+
+    def cover_image(self, post):
+        """ display image """
+        return format_html('<img src="{}" class="thumbnail"/>',
+                           post.images.first().url)
+
+    class Media:
+        """ load style files """
+        css = {
+            'all': ['app/css/styles.css']
+        }
 
 
 # register Destination to admin
 @admin.register(models.Destination)
 class DestinationAdmin(admin.ModelAdmin):
-    list_display = ['title', 'user', 'created_at']
+    list_display = ['title', 'user', 'created_at', 'iamge']
+
+    def iamge(self, destination):
+        """ display image """
+        return format_html('<img src="{}" class="thumbnail"/>',
+                           destination.images.first().url)
+
+    class Media:
+        """ load style files """
+        css = {
+            'all': ['app/css/styles.css']
+        }
 
 
 # register Comment to admin
